9dc806c2e084c148a22834daa181caa096a36237
Yinon Vahab <yinonv@gmail.com>
2019-11-26 14:53:00 +0200

    basic works - added random images

--- js/memory-game.js
+++ js/memory-game.js
@@ -48,32 +48,46 @@ checkMatch = (e) => {
 handleSelection = (e) => {
     let selected = selectedCards();
     if (selected == 1) {
-        e.target.style.cssText = `background-image: url(./img/${MemoryGame.level}/${e.target.num}.png)`;
+        e.target.style.cssText = `background-image: url(./img/${e.target.num}.png)`;
         e.target.classList.add('selected');
         setTimeout(() => {
             checkMatch(e);
         }, 700);
     } else if (selected == 0) {
-        e.target.style.cssText = `background-image: url(./img/${MemoryGame.level}/${e.target.num}.png)`;
+        e.target.style.cssText = `background-image: url(./img/${e.target.num}.png)`;
         e.target.classList.add('selected');
     }
 }
+getImages = () => {
+    let cardImages = new Array(MemoryGame.numberOfCards / 2);
+    let imageIndex = 0;
+    while (cardImages.includes(undefined)) {
+        let num = Math.floor(Math.random() * 30);
+        if (!cardImages.includes(num)) {
+            cardImages[imageIndex] = num;
+            imageIndex++;
+        }
+    }
+    return cardImages;
+}
 randomCards = () => {
-    let cardNum = new Array(MemoryGame.numberOfCards).fill(0);
+    let cardImages = getImages();
+    let cardNum = new Array(MemoryGame.numberOfCards/2).fill(0);
     let cardsOnBoard = 0;
     let i = 0;
     let num;
-    while (cardsOnBoard < 12) {
-        num = Math.floor(Math.random() * (MemoryGame.numberOfCards / 2));
-        while (cardNum[num] == 2) {
-            num = Math.floor(Math.random() * (MemoryGame.numberOfCards / 2));
+    while (cardsOnBoard < MemoryGame.numberOfCards) {
+        let randNum = Math.floor(Math.random() * (MemoryGame.numberOfCards / 2));
+        while (cardNum[randNum] == 2) {
+            randNum = Math.floor(Math.random() * (MemoryGame.numberOfCards / 2));
         }
+        num = cardImages[randNum];
         MemoryGame.cards[i].num = num;
         MemoryGame.cards[i].index = i;
         MemoryGame.cards[i].style.cssText = `background-image: url(./img/nba_card.png)`;
         MemoryGame.cards[i].addEventListener('click', handleSelection)
         i++;
-        ++cardNum[num];
+        ++cardNum[randNum];
         cardsOnBoard++;
     }
 }
