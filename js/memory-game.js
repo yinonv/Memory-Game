01e9f8c36bf0024837fca2799f7aa5916e0423b9
Yinon Vahab <yinonv@gmail.com>
2019-11-26 23:50:37 +0200

    added levels and design

--- js/memory-game.js
+++ js/memory-game.js
@@ -4,7 +4,10 @@ MemoryGame.numberOfCards = 12;
 MemoryGame.wrongCounter = 0;
 MemoryGame.cards = document.getElementsByClassName('card');
 MemoryGame.wrong = document.getElementById('wrong-answers');
-MemoryGame.showCard = true;
+MemoryGame.cardContainer = document.getElementById('card-container');
+MemoryGame.playMatchMusic = document.getElementById('match-music');
+MemoryGame.startMusic = document.getElementById('player');
+document.getElementById('mute').addEventListener('click', () => MemoryGame.startMusic.pause());
 selectedCards = () => {
     let selected = 0;
     for (let i = 0; i < MemoryGame.cards.length; i++) {
@@ -36,26 +39,36 @@ checkMatch = (e) => {
                 MemoryGame.cards[i].style.cssText = `background-image: url(./img/nba_card.png)`;
                 MemoryGame.wrong.innerText = `Wrong guesses: ${++MemoryGame.wrongCounter}`;
             } else {
+                if(!MemoryGame.startMusic.paused){
+                    MemoryGame.playMatchMusic.play();
+                }
                 e.target.classList.add('correct');
                 MemoryGame.cards[i].classList.add('correct');
                 if (userWon()) {
-                    alert(`You Won!, you had ${MemoryGame.wrongCounter} wrong guesses!`)
+                    MemoryGame.wrong.innerText = "";
+                    MemoryGame.startButton.innerText = "restart";
+                    MemoryGame.winGif.style.display = "block";
+                    MemoryGame.winTextTop.innerText = 'You Won!';
+                    MemoryGame.winTextBottom.innerText = `you had ${MemoryGame.wrongCounter} wrong guesses!`;
+                    deleteCards();
                 }
             }
         }
     }
 }
 handleSelection = (e) => {
-    let selected = selectedCards();
-    if (selected == 1) {
-        e.target.style.cssText = `background-image: url(./img/${e.target.num}.png)`;
-        e.target.classList.add('selected');
-        setTimeout(() => {
-            checkMatch(e);
-        }, 700);
-    } else if (selected == 0) {
-        e.target.style.cssText = `background-image: url(./img/${e.target.num}.png)`;
-        e.target.classList.add('selected');
+    if (!e.target.classList.contains('correct')) {
+        let selected = selectedCards();
+        if (selected == 1) {
+            e.target.style.cssText = `background-image: url(./img/${e.target.num}.png); background-size: contain`;
+            e.target.classList.add('selected');
+            setTimeout(() => {
+                checkMatch(e);
+            }, 700);
+        } else if (selected == 0) {
+            e.target.style.cssText = `background-image: url(./img/${e.target.num}.png); background-size: contain`;
+            e.target.classList.add('selected');
+        }
     }
 }
 getImages = () => {
@@ -72,7 +85,7 @@ getImages = () => {
 }
 randomCards = () => {
     let cardImages = getImages();
-    let cardNum = new Array(MemoryGame.numberOfCards/2).fill(0);
+    let cardNum = new Array(MemoryGame.numberOfCards / 2).fill(0);
     let cardsOnBoard = 0;
     let i = 0;
     let num;
@@ -91,6 +104,79 @@ randomCards = () => {
         cardsOnBoard++;
     }
 }
-randomCards();
-
+createCards = () => {
+    for (let i = 0; i < 4; i++) {
+        let rowCard = document.createElement('div');
+        rowCard.classList.add('row');
+        rowCard.classList.add('card-row');
+        MemoryGame.cardContainer.append(rowCard);
+        for (let j = 0; j < MemoryGame.numberOfCards / 4; j++) {
+            let divCard = document.createElement('div');
+            divCard.classList.add('card');
+            rowCard.append(divCard);
+        }
+    }
+}
+removeSelectedLevel = () => {
+    for (let i = 0; i < MemoryGame.levels.length; i++) {
+        MemoryGame.levels[i].classList.remove('level-selected');
+    }
+}
+selectLevel = (e) => {
+    removeSelectedLevel();
+    e.target.classList.add('level-selected');
+    switch (e.target.attributes[0].value) {
+        case "rookie": MemoryGame.numberOfCards = 12;
+            break;
+        case "advanced": MemoryGame.numberOfCards = 16;
+            break;
+        case "pro": MemoryGame.numberOfCards = 20;
+            break;
+    }
+}
+enterModal = () => {
+    deleteCards();
+    MemoryGame.wrongCounter = 0;
+    MemoryGame.startButton.innerText = "restart";
+    MemoryGame.winTextBottom.innerText = "";
+    MemoryGame.winTextTop.innerText = "";
+    MemoryGame.winGif.style.display = "none";
+    MemoryGame.startMusic.play();
+    MemoryGame.modal.style.display = 'block';
+}
+closeModal = () => {
+    MemoryGame.startButton.innerText = "Start";
+    MemoryGame.modal.style.display = 'none';
+}
+beginGame = () => {
+    MemoryGame.modal.style.display = 'none';
+    MemoryGame.wrong.style.visibility = 'visible';
+    createCards();
+    randomCards();
+}
+createModal = () => {
+    MemoryGame.modal = document.getElementsByClassName('modal')[0];
+    MemoryGame.closeModal = document.getElementsByClassName('close')[0];
+    MemoryGame.muteButton = document.getElementsByClassName('btn-secondary')[0];
+    MemoryGame.beginGameButton = document.getElementsByClassName('btn-primary')[0];
+    MemoryGame.levels = document.getElementsByClassName('level');
+    MemoryGame.startButton.addEventListener('click', enterModal);
+    MemoryGame.closeModal.addEventListener('click', closeModal);
+    MemoryGame.muteButton.addEventListener('click', () => MemoryGame.startMusic.pause());
+    for (let i = 0; i < MemoryGame.levels.length; i++) {
+        MemoryGame.levels[i].addEventListener('click', selectLevel);
+    }
+    MemoryGame.beginGameButton.addEventListener('click', beginGame)
+}
+deleteCards = () => {
+    document.querySelectorAll('.card').forEach(card => card.remove());
+}
+start = () => {
+    MemoryGame.startButton = document.getElementById('start-button');
+    MemoryGame.winGif = document.getElementById('win');
+    MemoryGame.winTextTop = document.getElementById('win-text-top');
+    MemoryGame.winTextBottom = document.getElementById('win-text-bottom');
+    createModal();
+}
+start();
 
